e8739be389bf76b348bf4abbd16bc05bde447ff6
TH3IS <nickiyde@gmail.com>
2024-02-19 10:26:04 +0100

    Remove dir

--- PowerShell/Fra lærer/GPO-Doku.ps1
+++
@@ -1,6 +0,0 @@
-﻿
-$dato = Get-Date -Format "yyyy-MM-dd___HH-mm-ss"
-
-Get-GPOReport -All -ReportType Html -Path c:\ny\GPO-$dato.html
-
-Get-GPO -All | select DisplayName, CreationTime, ModificationTime | sort ModificationTime | out-file c:\ny\GPO-$dato.txt
--- PowerShell/Fra lærer/PS_ASCII.ps1
+++
@@ -1,4 +0,0 @@
-﻿for ($i=0; $i -le 10000;$i++) {
-    $i.ToString() + ' '+ [char]$i | Out-File ascii.txt -Append
-
-}
