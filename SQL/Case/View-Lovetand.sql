ced1ae2e8ee7e4a0c04f5dd2c80c84ea8aa4c3e6
TH3IS <nickiyde@gmail.com>
2024-03-08 12:36:01 +0100

    Update View-Lovetand.sql

--- SQL/Case/View-Lovetand.sql
+++ SQL/Case/View-Lovetand.sql
@@ -16,9 +16,28 @@ GO
 -- Create the view in the specified schema
 CREATE VIEW Gartner.V_Maalinger_Per_Plantetype
 AS
-    -- body of the view
-    SELECT [Column1],
-        [Column2],
-        [Column3],
-    FROM Gartner.TableName
+   SELECT
+        TOP (100) PERCENT
+        Medhjaelper.Plantetyper.Plantetype,
+        Medhjaelper.Plantetyper.Plantebeskrivelse,
+        Gartner.Maaler.Maaleenhed,
+        FORMAT(Administration.Maaling.Tidspunkt, 'yyyy-MM') AS Tidspunkt,
+        CAST(AVG(Administration.Maaling.MaaltVaerdi) AS Decimal(10,2)) AS Gennemsnitstemperatur,
+        MIN(Administration.Maaling.MaaltVaerdi) AS Minimumstemperatur,
+        MAX(Administration.Maaling.MaaltVaerdi) AS Maximumtemperatur
+        FROM Medhjaelper.Plantetyper
+            LEFT JOIN Medhjaelper.Bord
+                ON Medhjaelper.Plantetyper.Plantetype = Medhjaelper.Bord.Plantetype
+            LEFT JOIN Gartner.BordMaaler
+                ON Medhjaelper.Bord.Drivhus = Gartner.BordMaaler.Drivhus
+                    AND Medhjaelper.Bord.Bord = Gartner.BordMaaler.Bord
+            LEFT JOIN Gartner.Maaler
+                ON Gartner.BordMaaler.MaalerNr = Gartner.Maaler.MaalerNr 
+                    AND Gartner.Maaler.MaalerType = 'Temperatur'
+            LEFT JOIN Administration.Maaling
+                ON Gartner.Maaler.MaalerNr = Administration.Maaling.MaalerNr
+            GROUP BY Medhjaelper.Plantetyper.Plantetype, Medhjaelper.Plantetyper.Plantebeskrivelse, Gartner.Maaler.Maaleenhed, FORMAT(Administration.Maaling.Tidspunkt, 'yyyy-MM')
+            ORDER BY Medhjaelper.Plantetyper.Plantetype ASC
+    
 GO
+
