fb2b314836b59de099d10395916131e480fcda89
TH3IS <nickiyde@gmail.com>
2024-03-08 12:35:40 +0100

    Create testspace.ps1

---
+++ PowerShell/Case/testspace.ps1
@@ -0,0 +1,5 @@
+$RemoteCSV = "\\10.192.20.35\Loevetand\*.csv"
+$LocalCSV = "C:\Users\Administrator\Documents\GitHub\Skole-Programmering\PowerShell\Case\csv\"
+$UsedCSV = "C:\Users\Administrator\Documents\GitHub\Skole-Programmering\PowerShell\Case\csv\used\"
+$Files = Get-item -Path "C:\Users\Administrator\Documents\GitHub\Skole-Programmering\PowerShell\Case\csv\*.csv"
+
